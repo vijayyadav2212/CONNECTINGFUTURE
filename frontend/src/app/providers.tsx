3f61499229694b1a5ed02c93a33dc9b2e21a214d
Vedant040106 <vedantangre268@gmail.com>
2026-02-09 12:03:12 +0530

    Settings Student

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -4,23 +4,25 @@ import { UserProvider } from "@auth0/nextjs-auth0/client";
 import { ThemeProvider } from "next-themes";
 import { AuthTokenProvider } from "../../contexts/AuthTokenContext";
 import { ProfileGate } from "@/contexts/ProfileGate";
+import { Toaster } from "react-hot-toast";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <UserProvider>
       <AuthTokenProvider>
-  <ProfileGate>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-  </ProfileGate>
+        <ProfileGate>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+            <Toaster position="bottom-right" toastOptions={{ duration: 3000 }} />
+          </ThemeProvider>
+        </ProfileGate>
       </AuthTokenProvider>
     </UserProvider>
   );
 }
- 
+
